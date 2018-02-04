0f847ece827dc15a2193b1a75ebc8ef33ef8f3fd
Nihal Gonsalves <nihal@nihalgonsalves.com>
2018-02-05 03:11:53 +0530

    Add authentication HOF

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,5 +1,32 @@
 import * as lodash from 'lodash';
 import authentication from './authentication';
 import health from './health';
+import user from './user';
 
-export default lodash.merge(health, authentication);
+const rawResolvers = lodash.merge(
+  health,
+  authentication,
+  user,
+);
+
+const requiresLogin = {
+  Query: {
+    me: true,
+  },
+};
+
+const resolvers = {
+  ...rawResolvers,
+  Query: lodash.mapValues(
+    rawResolvers.Query,
+    (resolver, name) =>
+      (root, args, context, info) => {
+        if (requiresLogin.Query[name] && !context.user) {
+          return Promise.reject('Authentication required');
+        }
+        return resolver(root, args, context, info);
+      },
+  ),
+};
+
+export default resolvers;
